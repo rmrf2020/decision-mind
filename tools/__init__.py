5513f3666615cc1722d09d9388ad3f474e8a6241
rmrf2020 <commonhc@outlook.com>
2024-12-13 20:14:58 +0800

    sub package scan support

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,19 +2,23 @@ import pkgutil
 import importlib
 import os
 
-# 动态发现当前包中的模块
 MODULES = {}
 
-# 获取当前包的名称
-package_name = os.path.basename(os.path.dirname(__file__))
 
-# 动态扫描包中的模块
-for _, module_name, _ in pkgutil.iter_modules([os.path.dirname(__file__)]):
-    full_module_name = f"{package_name}.{module_name}"
-    module = importlib.import_module(full_module_name)
-    if hasattr(module, "info"):
-        info = module.info()
-        MODULES[info.name] = [full_module_name, info]
+def scan_package(package_path, package_name):
+    """
+    """
+    for _, module_name, is_pkg in pkgutil.iter_modules([package_path]):
+        full_module_name = f"{package_name}.{module_name}"
+        module = importlib.import_module(full_module_name)
+        if hasattr(module, "info"):
+            info = module.info()
+            MODULES[info.name] = [full_module_name, info]
+
+        if is_pkg:
+            sub_package_path = os.path.join(package_path, module_name)
+            sub_package_name = f"{package_name}.{module_name}"
+            scan_package(sub_package_path, sub_package_name)
 
 
 async def router(name, method="handler", arguments: dict = None):
@@ -28,3 +32,7 @@ async def router(name, method="handler", arguments: dict = None):
         return await target_method(**arguments)
     else:
         return await target_method()
+
+
+current_package_name = os.path.basename(os.path.dirname(__file__))
+scan_package(os.path.dirname(__file__), current_package_name)
